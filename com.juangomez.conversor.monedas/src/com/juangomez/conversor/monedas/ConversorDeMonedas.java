816ae2ef8949cd3d4caf6ed209cec635b5e50a5f
elministroisrael <elministroisrael@gmail.com>
2025-11-28 20:41:53 -0500

    se agrego un menu utilizando el switch.

--- com.juangomez.conversor.monedas/src/com/juangomez/conversor/monedas/ConversorDeMonedas.java
+++ com.juangomez.conversor.monedas/src/com/juangomez/conversor/monedas/ConversorDeMonedas.java
@@ -10,24 +10,86 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+
 public class ConversorDeMonedas {
 
-    public static void main(String[] args) {
 
-        Scanner lectura = new Scanner(System.in);
-                System.out.println("Escriba los tres digitos correspondiente a la modeda base:");
-        var monedaBase = lectura.nextLine();
-        System.out.println("su moneda base es : " + monedaBase);
+    public static void main (String[] args) {
 
-        System.out.println("Escriba los tres digitos correspondiente a la modeda a cambiar: ");
-        var monedaCambio = lectura.nextLine();
-        System.out.println("su moneda base es : " + monedaCambio);
+      Scanner lectura = new Scanner(System.in);
+
+        int opcion;
+
+        do {
+            String monedaBase="";
+            String monedaCambio="";
+            exibirMenu();
+            opcion = lectura.nextInt();
+
+            switch (opcion) {
+
+                case 1 -> {
+                    monedaBase = "USD";
+                    monedaCambio = "ARS";
+                }
+                case 2 -> {
+                    monedaBase = "ARS";
+                    monedaCambio = "USD";
+                }
+                case 3 -> {
+                    monedaBase = "USD";
+                    monedaCambio = "BRL";
+                }
+                case 4 -> {
+                    monedaBase = "BRL";
+                    monedaCambio = "USD";
+                }
+                case 5 -> {
+                    monedaBase = "USD";
+                    monedaCambio = "COP";
+                }
+                case 6 -> {
+                    monedaBase = "COP";
+                    monedaCambio = "USD";
+                }
+                case 7 -> System.out.println("Saliendo del programa. ¡Gracias por usar el conversor!");
+                default -> System.out.println("Opción inválida. Intente nuevamente.");
 
 
-        System.out.println("Escriba los tres digitos correspondiente a la modeda a cambiar: ");
-        var dinero = Double.parseDouble(lectura.nextLine());
-        final var conversionRate = ConversorMonedaService.getExchangeRate(monedaBase, monedaCambio);
-        System.out.println("su cambio : " + (dinero * conversionRate));
+            }
+
+            System.out.println("Escriba la cantidad de dinero a cambiar: ");
+            var dinero = lectura.nextInt();
+            final var conversionRate = ConversorMonedaService.getExchangeRate(monedaBase, monedaCambio);
+            System.out.println("su cambio : " + (dinero * conversionRate));
+
+
+
+
+        } while (opcion != 7);
+
+        lectura.close();
+
+
+
+
+    }
+    public static void exibirMenu() {
+        System.out.println("""
+    ********************************************
+    Sea bienvenido/a al Conversor de Moneda =]
+    ********************************************
+
+    1) Dólar ==> Peso argentino
+    2) Peso argentino ==> Dólar
+    3) Dólar ==> Real brasileño
+    4) Real brasileño ==> Dólar
+    5) Dólar ==> Peso colombiano
+    6) Peso colombiano ==> Dólar
+    7) Salir
+
+    Elija una opción válida:
+    """);
     }
 
 
